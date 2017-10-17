9b349f7b39119832c955b07a79455766a73d7f00
Kevin Baker <kbaker@atomeq.com>
2017-10-17 17:23:35 -0400

    added api key

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-      'callback=initialize';
+      'callback=initialize&key=AIzaSyBdxNzJxJmDEUfeE62r2hAMEGsv-oZ602A';
   document.body.appendChild(script);
 }
 window.onload = loadScript;
